920d0ddd991805486bd1434d8083f2c613a7f455
Myles Braithwaite <me@mylesbraithwaite.com>
2011-04-05 13:18:46 -0400

    Updated the Test URLs for the new Django 1.3

--- src/contacts/testurls.py
+++ src/contacts/testurls.py
@@ -5,7 +5,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
 	url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-	url(r'^admin/(.*)', admin.site.root),
+	url(r'^admin/', include(admin.site.urls)),
 	url(r'^comments/', include('django.contrib.comments.urls')),
 	
 	url(r'^', include('contacts.urls')),
