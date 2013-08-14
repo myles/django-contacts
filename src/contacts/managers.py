5b46d5aef4c730f123a35a8cf2f5a9ea843f2f50
Myles Braithwaite <me@mylesbraithwaite.com>
2013-08-14 12:11:16 -0400

    Added some more common queries.

--- src/contacts/managers.py
+++ src/contacts/managers.py
@@ -4,10 +4,9 @@ from django.db.models import Manager, Q
 
 class SpecialDateManager(Manager):
 	
-	def get_dates(self, date=None):
+	def get_dates_for_day(self, date=None):
 		"""
-		Return a list of special dates for a given day even if the date is
-		recurring every year.
+		Return the list of speical days for a given day.
 		"""
 		
 		if not date:
@@ -16,6 +15,36 @@ class SpecialDateManager(Manager):
 		special_dates = self.get_query_set().filter(
 			Q(date=date) |
 			Q(every_year=True, date__month=date.month, date__day=date.day)
+		)
+		
+		return special_dates
+	
+	def get_dates_for_month(self, date=None):
+		"""
+		Return the list of speical days for a given month.
+		"""
+		
+		if not date:
+			date = datetime.date.today()
+		
+		special_dates = self.get_query_set().filter(
+			Q(date__month=date.month, date__year=date.year) |
+			Q(every_year=True, date__month=date.month)
+		)
+		
+		return special_dates
+	
+	def get_dates_for_year(self, date=None):
+		"""
+		Return the list of speical days for a given year.
+		"""
+		
+		if not date:
+			date = datetime.date.today()
+		
+		special_dates = self.get_query_set().filter(
+			Q(date__year=date.year) |
+			Q(every_year=True)
 		)
 		
 		return special_dates
