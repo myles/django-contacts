c983be9452c335759e84d8d735213913010f66ca
Erik Dykema <erik@caserails.com>
2014-01-12 14:26:59 -0500

    Update CI Scripts

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
 echo "Installing Build Requirements"
-pip install -r requirements.txt
+curl https://raw2.github.com/CaseRails/ci_scripts/master/ci_requirements.txt >> requirements.txt
+curl -O https://raw2.github.com/CaseRails/ci_scripts/master/build_artifacts.py
+curl -O https://raw2.github.com/CaseRails/ci_scripts/master/deploy.py
+curl -O https://raw2.github.com/CaseRails/ci_scripts/master/deploy_wheels.py
+curl -O https://raw2.github.com/CaseRails/ci_scripts/master/get_env_vars.py
+curl -O https://raw2.github.com/CaseRails/ci_scripts/master/retrieve.py
+sort -u requirements.txt > sorted_requirements.txt
+pip install -r sorted_requirements.txt
 python setup.py bdist_wheel
--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,1 @@
-curl https://raw2.github.com/CaseRails/ci_scripts/master/deploy.py > deploy.py
-curl https://raw2.github.com/CaseRails/ci_scripts/master/requirements.txt > deploy_requirements.txt
-
-pip install -r deploy_requirements.txt
 python deploy.py
